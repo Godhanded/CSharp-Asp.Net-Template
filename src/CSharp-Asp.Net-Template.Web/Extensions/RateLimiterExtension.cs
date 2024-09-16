9cb16bb92f0de87bf132976f5826743f80cc2170
Godand <aezeigwe2003@gmail.com>
2024-09-16 23:06:07 +0200

    fix(Web): Add RateLimit 429 Response Body

--- src/CSharp-Asp.Net-Template.Web/Extensions/RateLimiterExtension.cs
+++ src/CSharp-Asp.Net-Template.Web/Extensions/RateLimiterExtension.cs
@@ -12,18 +12,32 @@ namespace CSharp_Asp.Net_Template.Web.Extensions
                     rlOpt.AddConcurrencyLimiter("IpConcurrencyLimit", opt =>
                     {
                         opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-                        opt.PermitLimit = 3;
-                        opt.QueueLimit = 2;
+                        opt.PermitLimit = 1;
+                        opt.QueueLimit = 0;
                     });
                     rlOpt.AddFixedWindowLimiter("IpWindowLimit", opt =>
                     {
                         opt.AutoReplenishment = true;
-                        opt.PermitLimit = 6;
+                        opt.PermitLimit = 1;
                         opt.Window = TimeSpan.FromSeconds(1);
-                        opt.QueueLimit = 2;
+                        opt.QueueLimit = 0;
                         opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
                     });
+
                     rlOpt.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+                    rlOpt.OnRejected = async (ctx, _) =>
+                    {
+                        ctx.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                        ctx.HttpContext.Response.ContentType = "application/json";
+
+                        await ctx.HttpContext.Response.WriteAsJsonAsync(new
+                        {
+                            success = false,
+                            message = "Rate limit exceeded. Try again later.",
+                            statusCode = ctx.HttpContext.Response.StatusCode,
+                        });
+                    };
 
                     //rlOpt.AddPolicy("Customlimit", ctx =>
                     //    {
